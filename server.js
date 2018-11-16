a8c82f1b62245666e53c8f1d31fe686c624478f6
rao <415ec080@gmail.com>
2018-11-17 00:20:47 +0530

    V2

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var http=require('http');
-
+var fs=require('fs');
 http.createServer(function(req,res){
     res.writeHead(200);
-    res.write("hai");
-    res.end();
+    fs.createReadStream('./test.html').pipe(res);
 }).listen(process.env.PORT||8081,function(){
     console.log("8081");
 });
